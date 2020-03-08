d960e342b028f5d7fde4a03933c58e9d202575df
Laurent Shala <laurentshala@icloud.com>
2020-03-08 15:26:54 -0400

    create sign out button on home page

--- admin_panel/lib/Screens/Home/home.dart
+++ admin_panel/lib/Screens/Home/home.dart
@@ -1,8 +1,19 @@
+import 'package:admin_panel/Services/authentication_service.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return Row(
+      children: [
+        Text("Home"),
+        RaisedButton(
+          child: Text("Sign out"),
+          onPressed: () async {
+            AuthenticationService.signOut();
+          },
+        )
+      ]
+    );
   }
 }
