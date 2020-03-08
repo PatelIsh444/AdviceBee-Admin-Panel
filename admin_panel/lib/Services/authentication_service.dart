01114835648cb03ff4928d83245f0e6e45bed86b
Laurent Shala <laurentshala@icloud.com>
2020-03-08 14:57:32 -0400

    create helper method to sign in + create stream

--- admin_panel/lib/Services/authentication_service.dart
+++ admin_panel/lib/Services/authentication_service.dart
@@ -1,3 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthenticationService {
 
+  final _auth = FirebaseAuth.instance;
+
+  Stream<FirebaseUser> get userStream {
+    return _auth.onAuthStateChanged;
+  }
+
+  void signIn(email, password) async {
+    _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
 }
