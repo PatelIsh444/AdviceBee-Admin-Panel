3173669dfe0d5776f95196c9138da7c30c624777
Laurent Shala <laurentshala@icloud.com>
2020-03-08 15:07:18 -0400

    observe user data from stream & show correct page

--- admin_panel/lib/Screens/authentication_coordinator.dart
+++ admin_panel/lib/Screens/authentication_coordinator.dart
@@ -1,10 +1,19 @@
 import 'package:admin_panel/Screens/Authentication/login.dart';
 import 'package:admin_panel/Screens/Home/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AuthenticationCoordinator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LogIn();
+    final user = Provider.of<FirebaseUser>(context);
+
+    if (user == null) {
+      return LogIn();
+    }
+    else {
+      return Home();
+    }
   }
 }
