548007a5f39590ee5083da6d7f55fb95edfae20e
Laurent Shala <laurentshala@icloud.com>
2020-03-08 16:37:18 -0400

    create login form

--- admin_panel/lib/Screens/Authentication/login.dart
+++ admin_panel/lib/Screens/Authentication/login.dart
@@ -4,16 +4,49 @@ import 'package:flutter/material.dart';
 class LogIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text("Log in"),
-        RaisedButton(
-          child: Text("Sign in"),
-          onPressed: () async {
-            AuthenticationService.signIn("fx9612@wayne.edu", "ls530100");
-          },
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 220,
+          color: Colors.orangeAccent,
+          padding: EdgeInsets.all(25),
+          child: Column(
+            children: [
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Email"
+                      ),
+                      onChanged: (value) async {
+                      // TODO: setState
+                      },
+                    ),                
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Password"
+                      ),
+                      obscureText: true,
+                      onChanged: (value) async {
+                        // TODO: setState
+                      },
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 25),
+              MaterialButton(
+                child: Text("Log in"),
+                onPressed: () async {
+                  // TODO: Sign in
+                },
+              )
+            ],
+          ),
         )
-      ]
+      )
     );
   }
 }
