2567422cf92d070220daa3f6f3028c2514ff9d2b
Laurent Shala <laurentshala@icloud.com>
2020-03-08 15:06:47 -0400

    create stream provider for user data on main.dart

--- admin_panel/lib/main.dart
+++ admin_panel/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:admin_panel/Services/authentication_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
 import 'Screens/authentication_coordinator.dart';
 
@@ -9,8 +11,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AuthenticationCoordinator(),
+    return StreamProvider<FirebaseUser>.value(
+      value: AuthenticationService().userStream,
+      child: MaterialApp(
+        home: AuthenticationCoordinator(),
+      )
     );
   }
 }
