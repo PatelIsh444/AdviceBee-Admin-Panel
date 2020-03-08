3af63711b644a7b0db8ad7b0da642ab8d52b0d28
Laurent Shala <laurentshala@icloud.com>
2020-03-08 14:37:14 -0400

    import home.dart in AuthenticationCoordinator

--- admin_panel/lib/Screens/authentication_coordinator.dart
+++ admin_panel/lib/Screens/authentication_coordinator.dart
@@ -1,4 +1,5 @@
 import 'package:admin_panel/Screens/Authentication/login.dart';
+import 'package:admin_panel/Screens/Home/home.dart';
 import 'package:flutter/material.dart';
 
 class AuthenticationCoordinator extends StatelessWidget {
