d473960fc21b1be6438237dd228fdae18a725e60
Laurent Shala <laurentshala@icloud.com>
2020-03-08 15:29:16 -0400

    create log in button with debug credentials

--- admin_panel/lib/Screens/Authentication/login.dart
+++ admin_panel/lib/Screens/Authentication/login.dart
@@ -1,8 +1,19 @@
+import 'package:admin_panel/Services/authentication_service.dart';
 import 'package:flutter/material.dart';
 
 class LogIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Log In");
+    return Row(
+      children: [
+        Text("Log in"),
+        RaisedButton(
+          child: Text("Sign in"),
+          onPressed: () async {
+            AuthenticationService.signIn("fx9612@wayne.edu", "ls530100");
+          },
+        )
+      ]
+    );
   }
 }
