5e6bacd834c992babd18ade70c02850fc71f41a1
Laurent Shala <laurentshala@icloud.com>
2020-03-08 14:34:40 -0400

    create basic AuthenticationCoordinator widget

---
+++ admin_panel/lib/Screens/authentication_coordinator.dart
@@ -0,0 +1,9 @@
+import 'package:admin_panel/Screens/Authentication/login.dart';
+import 'package:flutter/material.dart';
+
+class AuthenticationCoordinator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LogIn();
+  }
+}
