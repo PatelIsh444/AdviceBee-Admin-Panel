a6282194eb953696c5e1f4fcc8ad0c9174aeb96a
Laurent Shala <laurentshala@icloud.com>
2020-03-09 17:48:13 -0400

    rearrange widgets in build method

--- admin_panel/lib/Screens/Home/home.dart
+++ admin_panel/lib/Screens/Home/home.dart
@@ -19,34 +19,17 @@ class _HomeState extends State<Home> {
     final accountEmail = (user != null) ? user.email ??  "Unknown Email" : "Unknown Email";
 
     return NavigationRail(
+      title: Text("Admin Panel"),
       drawerHeaderBuilder: (context) {
         return Column(
           children: <Widget>[
             UserAccountsDrawerHeader(
               accountName: Text(accountName),
-              accountEmail: Text(accountEmail)
+              accountEmail: Text(accountEmail),
             ),
           ],
         );
       },
-      drawerFooterBuilder: (context) {
-        return Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.arrow_back),
-              title: Text("Sign Out"),
-              onTap: () async {
-                AuthenticationService.signOut();
-              },
-            ),
-          ],
-        );
-      },
-      currentIndex: _currentIndex,
-      onTap: (val) {
-        if (mounted) setState(() => _currentIndex = val);
-      },
-      title: Text("Admin Panel"),
       body: IndexedStack(
         index: _currentIndex,
         children: <Widget>[
@@ -64,6 +47,23 @@ class _HomeState extends State<Home> {
           icon: Icon(Icons.report),
         )
       ],
+      currentIndex: _currentIndex,
+      onTap: (val) {
+        if (mounted) setState(() => _currentIndex = val);
+      },
+      drawerFooterBuilder: (context) {
+        return Column(
+          children: [
+            ListTile(
+              leading: Icon(Icons.arrow_back),
+              title: Text("Sign Out"),
+              onTap: () async {
+                AuthenticationService.signOut();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
