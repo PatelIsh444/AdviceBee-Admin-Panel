e193f0b5029dacbc4faba0b14a7953e975582536
Laurent Shala <laurentshala@icloud.com>
2020-03-12 02:56:50 -0400

    update reports UI to display collection of reports

--- admin_panel/lib/Screens/Reports/reports.dart
+++ admin_panel/lib/Screens/Reports/reports.dart
@@ -8,25 +8,21 @@ class Reports extends StatelessWidget {
       stream: Firestore.instance.collection("reports").snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {  
         if (snapshot.hasData) {
-          return GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-            padding: const EdgeInsets.all(4.0),
-            itemCount: snapshot.data.documents.length,
-            
-            itemBuilder: (BuildContext context, int index) {
+          return Wrap(
+            children: snapshot.data.documents.map((element) {
               return Container(
                 color: Colors.white,
-                width: 50,
-                height: 50,
+                width: 150,
+                height: 100,
                 child: Padding(
                   padding: EdgeInsets.all(12),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(snapshot.data.documents[index]["postTitle"] ?? "N/A"),
+                      Text(element["postTitle"] ?? "N/A"),
                       SizedBox(height: 8),
                       FutureBuilder(
-                        future: Firestore.instance.collection('users').document(snapshot.data.documents[index]["postCreatedBy"]).get(), 
+                        future: Firestore.instance.collection('users').document(element["postCreatedBy"]).get(), 
                         builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {  
                           if (snapshot.hasData) {
                             return Text(snapshot.data.data["displayName"]);
@@ -35,12 +31,12 @@ class Reports extends StatelessWidget {
                             return Text("Loading post creator's name");
                           }
                         }, 
-                      )
-                    ],
+                      )    
+                    ]              
                   )
-                ) 
+                )
               );
-            }
+            }).toList()
           );
         }
         else {
