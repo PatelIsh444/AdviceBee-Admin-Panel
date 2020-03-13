8f2c4832fb4b00cd91015617a90c455f29d84f45
Laurent Shala <laurentshala@icloud.com>
2020-03-13 01:07:37 -0400

    add on hover drop shadows

--- admin_panel/lib/Screens/Reports/reports.dart
+++ admin_panel/lib/Screens/Reports/reports.dart
@@ -1,7 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Reports extends StatelessWidget {
+class Reports extends StatefulWidget {
+  @override
+  _ReportsState createState() => _ReportsState();
+}
+
+class _ReportsState extends State<Reports> {
+  bool isHovering = false;
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -12,58 +18,59 @@ class Reports extends StatelessWidget {
             children: snapshot.data.documents.map((element) {
               return MouseRegion(
                 onEnter: (event) {
-                  print("Enter");
+                  setState(() => isHovering = true);
                 },
                 onExit: (event) {
-                  print("Exit");
+                  setState(() => isHovering = false);
                 },
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(14.0),
-                  child: Container(
-                    color: Colors.white,
-                    width: 250,
-                    child: Padding(
-                      padding: EdgeInsets.all(12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            element["postTitle"] ?? "N/A",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 30,
-                            ),
+                child: Container(
+                  decoration: BoxDecoration(
+                      boxShadow: isHovering ? kElevationToShadow[1] : [],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                  width: 250,
+                  child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          element["postTitle"] ?? "N/A",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 30,
                           ),
-                          SizedBox(height: 8),
-                          FutureBuilder(
-                            future: Firestore.instance.collection('users').document(element["postCreatedBy"]).get(), 
-                            builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {  
-                              if (snapshot.hasData) {
-                                return Text(
-                                  "Posted By: " + snapshot.data.data["displayName"],
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w200,
-                                    fontSize: 14,
-                                    color: Colors.grey
-                                  ),
-                                );
-                              }
-                              else {
-                                return Text(
-                                  "Loading post creator's name",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w200,
-                                    fontSize: 14,
-                                    color: Colors.grey
-                                  ),
-                                );
-                              }
-                            }, 
-                          )
-                        ]
-                      )
+                        ),
+                        SizedBox(height: 8),
+                        FutureBuilder(
+                          future: Firestore.instance.collection('users').document(element["postCreatedBy"]).get(), 
+                          builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {  
+                            if (snapshot.hasData) {
+                              return Text(
+                                "Posted By: " + snapshot.data.data["displayName"],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w200,
+                                  fontSize: 14,
+                                  color: Colors.grey
+                                ),
+                              );
+                            }
+                            else {
+                              return Text(
+                                "Loading post creator's name",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w200,
+                                  fontSize: 14,
+                                  color: Colors.grey
+                                ),
+                              );
+                            }
+                          }, 
+                        )
+                      ]
                     )
-                  ),
+                  )
                 ),
               );
             }).toList()
