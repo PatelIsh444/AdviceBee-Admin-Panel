adf27df6ddc77c373fcdbc0b7518c1d757584f8d
Laurent Shala <laurentshala@icloud.com>
2020-03-14 14:18:18 -0400

    add overview header title

--- admin_panel/lib/Screens/Overview/overview.dart
+++ admin_panel/lib/Screens/Overview/overview.dart
@@ -5,56 +5,69 @@ class Overview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(14.0),
-            child: Container(
-              color: Colors.white,
-              width: 150,
-              height: 100,
-              child: Padding(
-                padding: EdgeInsets.all(12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Reports",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w200,
-                        fontSize: 20,
-                        color: Colors.grey
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    StreamBuilder(
-                      stream: Firestore.instance.collection("reports").snapshots(), 
-                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                        final style = TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 38
-                        );
+          Text(
+            "Overview",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 35,
+            )
+          ),
+          SizedBox(height: 12),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(14.0),
+                child: Container(
+                  color: Colors.white,
+                  width: 150,
+                  height: 100,
+                  child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Reports",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w200,
+                            fontSize: 20,
+                            color: Colors.grey
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        StreamBuilder(
+                          stream: Firestore.instance.collection("reports").snapshots(), 
+                          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                            final style = TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 38
+                            );
 
-                        if (snapshot.hasData) {
-                          return Text(
-                            snapshot.data.documents.length.toString(),
-                            style: style,
-                          );
-                        }
-                        else {
-                          return Text(
-                            "--",
-                            style: style,
-                          );
-                        }
-                      },
+                            if (snapshot.hasData) {
+                              return Text(
+                                snapshot.data.documents.length.toString(),
+                                style: style,
+                              );
+                            }
+                            else {
+                              return Text(
+                                "--",
+                                style: style,
+                              );
+                            }
+                          },
+                        )
+                      ],
                     )
-                  ],
-                )
-              ) 
-            ),
-          )
+                  ) 
+                ),
+              )
+            ],
+          ),
         ],
       )
     );
