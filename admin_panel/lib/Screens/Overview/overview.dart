3488fa05dc84b24b1ecd4acf20e0f08aaa73dfcd
Laurent Shala <laurentshala@icloud.com>
2020-03-14 14:19:13 -0400

    move view generation to a method

--- admin_panel/lib/Screens/Overview/overview.dart
+++ admin_panel/lib/Screens/Overview/overview.dart
@@ -4,72 +4,76 @@ import 'package:flutter/material.dart';
 class Overview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            "Overview",
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 35,
-            )
-          ),
-          SizedBox(height: 12),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(14.0),
-                child: Container(
-                  color: Colors.white,
-                  width: 150,
-                  height: 100,
-                  child: Padding(
-                    padding: EdgeInsets.all(12),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Reports",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w200,
-                            fontSize: 20,
-                            color: Colors.grey
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        StreamBuilder(
-                          stream: Firestore.instance.collection("reports").snapshots(), 
-                          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                            final style = TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 38
-                            );
+    return _generateOverview();
+  }
 
-                            if (snapshot.hasData) {
-                              return Text(
-                                snapshot.data.documents.length.toString(),
-                                style: style,
-                              );
-                            }
-                            else {
-                              return Text(
-                                "--",
-                                style: style,
-                              );
-                            }
-                          },
-                        )
-                      ],
-                    )
-                  ) 
-                ),
-              )
-            ],
-          ),
-        ],
-      )
-    );
+  Container _generateOverview() {
+    return Container(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Overview",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 35,
+          )
+        ),
+        SizedBox(height: 12),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(14.0),
+              child: Container(
+                color: Colors.white,
+                width: 150,
+                height: 100,
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Reports",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w200,
+                          fontSize: 20,
+                          color: Colors.grey
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      StreamBuilder(
+                        stream: Firestore.instance.collection("reports").snapshots(), 
+                        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                          final style = TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 38
+                          );
+
+                          if (snapshot.hasData) {
+                            return Text(
+                              snapshot.data.documents.length.toString(),
+                              style: style,
+                            );
+                          }
+                          else {
+                            return Text(
+                              "--",
+                              style: style,
+                            );
+                          }
+                        },
+                      )
+                    ],
+                  )
+                ) 
+              ),
+            )
+          ],
+        ),
+      ],
+    )
+  );
   }
 }
