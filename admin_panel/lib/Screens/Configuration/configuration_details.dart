7ceee2ea2b6cbad331da20c89993c1ae55525c41
Laurent Shala <laurentshala@icloud.com>
2020-03-28 16:21:11 -0400

    create form to update number of posts per rank

--- admin_panel/lib/Screens/Configuration/configuration_details.dart
+++ admin_panel/lib/Screens/Configuration/configuration_details.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ConfigurationDetails extends StatefulWidget {
@@ -19,8 +20,71 @@ class _ConfigurationDetailsState extends State<ConfigurationDetails> {
   }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    List<Widget> children = [
+      Text(
+      "Set daily number of posts per rank:",
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 28,
+        )
+      ),
+      SizedBox(height: 32,)
+    ];
+
+    dailyQuestionsLimitMap.forEach((key, value) {
+      children.add(_generateTextFormField(key, value));
+      children.add(SizedBox(height: 32,));
+    });
+    
+    children.add(
+      RaisedButton(
+        child: Text("confirm"), 
+        onPressed: () async {
+          await _updateConfigWithUpdatedDailyNumberOfPostsPerRank(dailyQuestionsLimitMap);
+          Navigator.of(context).pop(true);
+        } 
+      )
     );
+
+    return Padding(
+      padding: const EdgeInsets.all(18),
+      child: SingleChildScrollView(
+        child: Form(
+          child: Column(
+            children: children,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _generateTextFormField(String key, dynamic value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          key,
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          )
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: "Enter daily number of posts limit"
+          ),
+          initialValue: value.toString(),
+          onChanged: (value) async {
+            setState(() => dailyQuestionsLimitMap[key] = value);
+          },
+        ),
+      ],
+    );
+  }
+
+  Future<void> _updateConfigWithUpdatedDailyNumberOfPostsPerRank(Map<String, dynamic> dailyQuestionsLimitMap) async {
+    Firestore.instance.collection("configuration").document("config").updateData({
+      "dailyQuestionsLimit": dailyQuestionsLimitMap
+    });
   }
 }
