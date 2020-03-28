810f0db70ea4dcb4b24e257254e66cbc1634501b
Laurent Shala <laurentshala@icloud.com>
2020-03-28 15:31:47 -0400

    create ConfigurationDetails skeleton

---
+++ admin_panel/lib/Screens/Configuration/configuration_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConfigurationDetails extends StatefulWidget {
+  @override
+  _ConfigurationDetailsState createState() => _ConfigurationDetailsState();
+}
+
+class _ConfigurationDetailsState extends State<ConfigurationDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
