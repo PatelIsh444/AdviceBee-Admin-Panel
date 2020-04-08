c2ae958a0f1f1bb60ff3ad4a0fde9bda2d6e341c
Mustafa Ahmed <fo8197@wayne.edu>
2020-04-08 01:21:42 -0400

    Add color or label to TYPE of report on the report cell Fixed

--- admin_panel/lib/Screens/Reports/report_cell.dart
+++ admin_panel/lib/Screens/Reports/report_cell.dart
@@ -25,6 +25,7 @@ class ReportCell extends StatelessWidget {
             _generateReportedPostHeader(element),
             SizedBox(height: 8),
             _generateUserDisplayName(element),
+            _generateReportsCategoryReason(element),
             _generateActionRow(context, element)
           ]
         )
@@ -69,6 +70,43 @@ class ReportCell extends StatelessWidget {
           );
         }      
       }, 
+    );
+  }
+
+  Widget _generateReportsCategoryReason(DocumentSnapshot element) {
+    String reasons = "";
+    return StreamBuilder(
+        stream: Firestore.instance.collection('reports').document(element.documentID).collection('ReportedUsers').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            snapshot.data.documents.forEach((e) {
+              reasons = (e.data["reasons"] as List).join(" & ");
+            });
+            Color colorReason = Colors.grey;
+            if(reasons.contains('Illegal')){
+              colorReason = Colors.redAccent;
+            }else if(reasons.contains('Uncivil')){
+              colorReason = Colors.deepOrange;
+            }else if(reasons.contains('Spam')){
+              colorReason = Colors.orangeAccent;
+            }else if(reasons.contains('Not relevant')){
+              colorReason = Colors.cyan;
+            }else{
+              colorReason = Colors.grey;
+            }
+            return Text(
+              reasons,
+              style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 14,
+                  color: colorReason
+              ),
+            );
+          }
+          else {
+            return Text("Loading report category...");
+          }
+        }
     );
   }
 
